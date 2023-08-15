856603ccf952026cb51e0831edbfb6b9d72389bf
Mohamed Snoussi <sooniic@live.com>
2023-08-15 23:47:55 +0200

    removed not needed middlewares

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -43,13 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "django_site.urls"
@@ -114,9 +106,9 @@ LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
 
-USE_I18N = True
+USE_I18N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
